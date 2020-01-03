129f5fc0b518d2b4ea5e865c4c6c11ef3d6f9058
autumn_prince <nguyenquangminh2912@gmail.com>
2020-01-03 16:58:03 +0700

    Trien khai Entity 01

---
+++ QMCoreApp.Infrastructure/SharedKernel/DomainEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QMCoreApp.Infrastructure.SharedKernel
+{
+    public class DomainEntity<T> //Kiểu T để tùy biến kiểu dữ liệu của Id > Kiểu Generic(Kiểu tạm)
+    {
+        public T Id { get; set; }
+
+        /// <summary>
+        /// True If Identity have a Identity
+        /// </summary>
+        /// <returns></returns>
+        public bool IsTransient()
+        {
+            return Id.Equals(default(T));
+        }
+
+    }
+}
